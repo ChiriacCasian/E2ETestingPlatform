f19354b7507f3b7dde5a11d57bb7aaadacc4ad9d
sebastian <sebastian@TeamCity>
2025-07-14 17:26:36 +0200

    general settings of 'SixthGeneratedBuild9' build configuration were updated (TeamCity change in 'Second_project' project)

--- .teamcity/patches/buildTypes/Journey_SixthGeneratedBuild9.kts
+++ .teamcity/patches/buildTypes/Journey_SixthGeneratedBuild9.kts
@@ -15,7 +15,7 @@ create(DslContext.projectId, BuildType({
     id("Journey_SixthGeneratedBuild9")
     name = "SixthGeneratedBuild9"
 
-    artifactRules = "insights.csv"
+    artifactRules = "*.csv"
 
     params {
         param("journeyName", "ceva")
