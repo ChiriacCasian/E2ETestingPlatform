0d29401e7a3c7e769c98a17532f1691dd59c1079
sebastian <sebastian@TeamCity>
2025-07-14 17:20:21 +0200

    'SixthGeneratedBuild9' build configuration settings were updated (TeamCity change in 'Second_project' project)

--- .teamcity/patches/buildTypes/Journey_SixthGeneratedBuild9.kts
+++ .teamcity/patches/buildTypes/Journey_SixthGeneratedBuild9.kts
@@ -30,6 +30,7 @@ create(DslContext.projectId, BuildType({
         script {
             name = "Save Report"
             id = "TEMPLATE_RUNNER_2"
+            enabled = false
             scriptContent = "ls -alR > folder_tree-after-download.log"
         }
     }
