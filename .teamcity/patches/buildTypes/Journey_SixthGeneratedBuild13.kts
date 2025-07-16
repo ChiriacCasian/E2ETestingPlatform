5f13a98ebbd64b49303eaa183ec0bc814676f4cc
sebastian <sebastian@TeamCity>
2025-07-16 10:12:47 +0200

    artifact dependencies of 'SixthGeneratedBuild13' build configuration were updated (TeamCity change in 'Second_project' project)

--- .teamcity/patches/buildTypes/Journey_SixthGeneratedBuild13.kts
+++ .teamcity/patches/buildTypes/Journey_SixthGeneratedBuild13.kts
@@ -22,7 +22,7 @@ create(DslContext.projectId, BuildType({
         artifacts(RelativeId("JourneyMakerV1")) {
             id = "ARTIFACT_DEPENDENCY_7"
             buildRule = build("80")
-            artifactRules = "*.txt=>scripts/theScript.txt"
+            artifactRules = "*.txt"
         }
     }
 
