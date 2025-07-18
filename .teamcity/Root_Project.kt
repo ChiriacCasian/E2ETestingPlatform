af1dc47bc9e1e39b0725f0b79a188256e2ec6761
sebastian.chiriac <sebastian.chiriac@jetbrains.com>
2025-07-18 11:04:21 +0200

    add buildReportTab to project

---
+++ .teamcity/Root_Project.kt
@@ -0,0 +1,19 @@
+import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.projectFeatures.buildReportTab
+
+object Root_Project : Project({
+
+    vcsRoot(HttpsGithubComChiriacCasianTCBuildServerGit)
+
+    buildType(JourneyMakerV1)
+
+    template(JourneyExecutorTemplate)
+
+    features {
+        buildReportTab {
+            id = "REPORT_TAB"
+            title = "Report"
+            startPage = "report/index.html"
+        }
+    }
+})
