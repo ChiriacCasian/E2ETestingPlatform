5ddb05564042a801d74686ef69d293d26f256b0b
sebastian.chiriac <sebastian.chiriac@jetbrains.com>
2025-07-30 15:26:55 +0200

    remove buildType for custom teamcity plugin

--- .teamcity/RootProject.kt
+++ .teamcity/RootProject.kt
@@ -3,17 +3,14 @@ import journeyGeneration.JourneyGenerator
 import jetbrains.buildServer.configs.kotlin.Project
 import jetbrains.buildServer.configs.kotlin.projectFeatures.buildReportTab
 import journeyExecution.JourneyExecutor
-import webpagesPlugin.WebpagesPlugin
 
 object RootProject : Project({
 
     vcsRoot(TCBuildServerGit)
     vcsRoot(JourneyExecutorPodInfraV2Git)
-    vcsRoot(WebpagesPluginGit)
 
     buildType(JourneyGenerator)
     buildType(JourneyExecutor)
-    buildType(WebpagesPlugin)
 
     template(JourneyTemplate)
 
--- .teamcity/TCBuildServerVcsRoots.kt
+++ .teamcity/TCBuildServerVcsRoots.kt
@@ -21,14 +21,3 @@ object JourneyExecutorPodInfraV2Git : GitVcsRoot({
     }
     branchSpec = "refs/heads/*" /// this is the vcs trigger
 })
-
-object WebpagesPluginGit : GitVcsRoot({
-    name = "https://github.com/ChiriacCasian/WebpagesInTeamcityPlugin.git"
-    url = "https://github.com/ChiriacCasian/WebpagesInTeamcityPlugin"
-    branch = "refs/heads/master"
-    authMethod = password {
-        userName = "ChiriacCasian"
-        password = "credentialsJSON:9b4689e5-3c42-4bfb-843e-7d0d1a023a38"
-    }
-    branchSpec = "refs/heads/*"
-})
