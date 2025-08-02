afd5b8038da8906af43595360ac5b6589b6e35e6
ChiriacCasian <chiriaccasian@gmail.com>
2025-08-02 15:56:11 +0300

    reformat codegen server configuration logic

--- .teamcity/journeyGeneration/JourneyGenerator.kt
+++ .teamcity/journeyGeneration/JourneyGenerator.kt
@@ -18,8 +18,8 @@ object JourneyGenerator : BuildType({
 
     params {
         /// we may want to split generators on type, for example WEB_GENERATOR, or ANDROID_GENERATOR, depending on what phone/emulator they have access to
-        param("CODEGEN_SERVER_URL", "http://host.docker.internal:3002/code")
-        param("JOURNEY_TYPE", "WEB")
+        param("env.CODEGEN_SERVER_URL", "http://host.docker.internal:3002/code")
+        param("env.JOURNEY_TYPE", "WEB")
         param("env.JOURNEY_NAME", "Journey")
         /// TODO: move this token to secrets vault
         param("env.GIT_PAT_TOKEN", "eyJ0eXAiOiAiVENWMiJ9.OGEtUUxCVnBZN3NrY2NlWHdzdl9yMmZLM0ln.ZjUzNDI0NjUtOWIwZS00YjQyLThlZGQtZjEwNDZhMmEzNzI4")
