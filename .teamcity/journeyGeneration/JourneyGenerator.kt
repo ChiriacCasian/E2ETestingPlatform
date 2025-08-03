0d216b7d7544169d3bfd5d3267f3b275ece3e152
ChiriacCasian <chiriaccasian@gmail.com>
2025-08-03 19:02:20 +0300

    add new CODEGEN_FRONTEND_URL parameter for journy generation

--- .teamcity/journeyGeneration/JourneyGenerator.kt
+++ .teamcity/journeyGeneration/JourneyGenerator.kt
@@ -19,10 +19,9 @@ object JourneyGenerator : BuildType({
     params {
         /// we may want to split generators on type, for example WEB_GENERATOR, or ANDROID_GENERATOR, depending on what phone/emulator they have access to
         param("env.CODEGEN_SERVER_URL", "http://host.docker.internal:3002/code")
+        param("env.CODEGEN_FRONTEND_URL", "http://localhost:4200")
         param("env.JOURNEY_TYPE", "WEB")
         param("env.JOURNEY_NAME", "Journey")
-        /// TODO: move this token to secrets vault
-//        param("env.GIT_PAT_TOKEN", "eyJ0eXAiOiAiVENWMiJ9.OGEtUUxCVnBZN3NrY2NlWHdzdl9yMmZLM0ln.ZjUzNDI0NjUtOWIwZS00YjQyLThlZGQtZjEwNDZhMmEzNzI4")
         param("env.TEAMCITY_SERVER_URL", "%teamcity.serverUrl%")
         /// MANUALLY get a project's id from settings General Project ID
         param("env.PROJECT_PATH_ID", "%teamcity.project.id%")
